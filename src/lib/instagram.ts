d0dc9b19e76a1c3c731dc28077c1a55d1fc37949
Sujithkrys <thalathotysujith@gmail.com>
2026-09-03 12:02:00 +0530

    Fix Instagram Auth URL to use api.instagram.com

--- src/lib/instagram.ts
+++ src/lib/instagram.ts
@@ -73,7 +73,7 @@ export function getInstagramAuthUrl(baseUrl?: string, state?: string): string {
     params.append("state", state);
   }
 
-  return `https://www.instagram.com/oauth/authorize?${params.toString()}`;
+  return `https://api.instagram.com/oauth/authorize?${params.toString()}`;
 }
 
 /**
